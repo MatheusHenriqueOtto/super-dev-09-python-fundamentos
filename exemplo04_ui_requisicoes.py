213bc5619e402e7768ac4f19700858f052aac3de
matheus <matheusotto1906@gmail.com>
2026-07-11 18:41:40 -0300

    Atualizando exemplos, terminado a atividade de requisição de empresa, adicionado filtros de pesquisa

--- exemplo04_ui_requisicoes.py
+++ exemplo04_ui_requisicoes.py
@@ -1,6 +1,12 @@
-import requests
+# GUI => Graphical User Interface
+# UI => User Interface
+
+# Instalar a biblioteca rich: `pip install rich`
+# Instalar a biblioteca questionary: `pip install questionary`
+
 from rich.console import Console
 from rich.table import Table
+import requests
 import questionary
 import os
 
@@ -17,16 +23,20 @@ def menu():
 
     menu_escolhido = ""
     while menu_escolhido != "Sair":
-        menu_escolhido =  questionary.select("Menu", choices=opcoes_menu).ask()
+        menu_escolhido = questionary.select("Menu", choices=opcoes_menu).ask()
         limpar_terminal()
 
         if menu_escolhido == "Consultar Categorias":
             consultar_categorias()
         elif menu_escolhido == "Cadastrar Categoria":
             cadastrar_categoria()
+        elif menu_escolhido == "Apagar Categoria":
+            apagar_categoria()
         
         if menu_escolhido != "Sair":
-            questionary.press_any_key_to_continue("Pressione quase qualquer tecla para continuar...").ask()
+            questionary.press_any_key_to_continue(
+                "Digite qualquer tecla para continuar..."
+            ).ask()
             limpar_terminal()
 
 
@@ -34,6 +44,7 @@ def limpar_terminal():
     os.system("cls")
 
 
+# Consultar categorias
 def consultar_categorias():
     url = f"{url_base}/api/v1/biblioteca/categorias"
 
@@ -41,11 +52,11 @@ def consultar_categorias():
 
     console = Console()
 
-    table = Table(show_header = True, header_style="bold blue", show_lines=True)
-    table.add_column("Codigo")
+    table = Table(show_header=True, header_style="bold blue", show_lines=True)
+    table.add_column("Código")
     table.add_column("Nome")
 
-    categorias =  resposta.json()
+    categorias = resposta.json()
 
     for categoria in categorias:
         table.add_row(str(categoria["id"]), categoria["nome"])
@@ -55,31 +66,33 @@ def consultar_categorias():
 
 def cadastrar_categoria():
     url = f"{url_base}/api/v1/biblioteca/categorias"
-    nome_categoria: str =  questionary.text("Digite o nome da categoria").ask()
+    nome_categoria: str = questionary.text("Digite o nome da categoria:").ask()
 
     categoria = {
         "nome": nome_categoria
     }
 
     resposta = requests.post(url, json=categoria)
-
+    # print("Status Code: ", resposta.status_code)
     if resposta.status_code == 201:
         print("Categoria cadastrada com sucesso")
     else:
-        print("Não foi possivel cadastrar a categoria")
+        print("Não foi possível cadastrar a categoria")
 
 
 def apagar_categoria():
-    id = int(questionary.text("Digite o codigo da categoria para apagar: ").ask())
+    id = int(questionary.text("Digite o código da categoria para apagar:").ask())
     url = f"{url_base}/api/v1/biblioteca/categorias/{id}"
 
-    resposta =  requests.delete(url)
+    resposta = requests.delete(url)
 
     if resposta.status_code == 204:
         print("Categoria apagada com sucesso")
     elif resposta.status_code == 404:
-        print("Não foi possivel encontrar a categoria com este codigo")
+        print("Não foi possível encontrar a categoria com este código")
     else:
-        print("Não foi possivel apagar a categoria")
+        print("Não foi possível apagar a categoria")
+    # print("Status Code:", resposta.status_code)
+
 
 menu()
--- exercicios_requisicoes_empresa.py
+++ exercicios_requisicoes_empresa.py
@@ -1,21 +1,194 @@
+import os
 import requests
 import questionary  
-
+from rich.console import Console
+from rich.table import Table
 
 url_base = "https://api.franciscosensaulas.com"
+url = f"{url_base}/api/v1/empresa"
 
 def menu():
     opcoes_menu = [
-        "Consultar",
+        "Consultar Todos",
+        "Consultar por id",
+        "Consultar por nome",
+        "Consultar por cnpj",
         "Cadastrar",
         "Editar",
-        "Apagar"
+        "Apagar",
+        "Sair"
     ]
 
     menu_escolhido = ""
 
     while menu_escolhido != "Sair":
-        menu_escolhido = questionary.select("Menu", choice=opcoes_menu).ask
+        menu_escolhido = questionary.select("Menu", choices=opcoes_menu).ask()
+        limpar_terminal()
+
+        if menu_escolhido == "Consultar Todos":
+            consultar_empresas()
+        elif menu_escolhido == "Consultar por id":
+            consultar_empresa_id()
+        elif menu_escolhido == "Consultar por nome":
+            consultar_empresa_nome()
+        elif menu_escolhido == "Consultar por cnpj":
+            consultar_empresa_cnpj()
+        elif menu_escolhido == "Cadastrar":
+            cadastrar_empresa()
+        elif menu_escolhido == "Editar":
+            editar_empresa()
+        elif menu_escolhido == "Apagar":
+            apagar_empresa()
+        
+        if menu_escolhido != "Sair":
+            questionary.press_any_key_to_continue(
+                "Digite qualquer tecla para continuar..."
+            ).ask()
+            limpar_terminal()
+
+
+
+def apagar_empresa():
+    id = int(questionary.text("Digite o id da empresa para apagar: ").ask())
+    url_id = f"{url}/{id}"
+
+    resposta = requests.delete(url_id)
+
+    if resposta.status_code == 204:
+        print("Sucesso ao apagar a empresa")
+    elif resposta.status_code == 404:
+        print("Não foi possivel encontrar a empresa com esse id")
+    else:
+        print("Não foi possivel apagar a empresa!")
+
+
+def editar_empresa():
+    id = int(questionary.text("Digite o id da empresa para aleterar: ").ask())
+    url_api = f"{url}/{id}"
+
+    nome_empresa: str = questionary.text("Digite o nome da empresa: ").ask()
+    cnpj_empresa: str = questionary.text("Digite o CNPJ da empresa: ").ask()
+
+    empresa = {
+        "nome": nome_empresa,
+        "cnpj": cnpj_empresa
+    }
+
+    resposta = requests.put(url_api, json=empresa)
+    if resposta.status_code == 204:
+        print("Empresa alterada com sucesso")
+    elif resposta.status_code == 404:
+        print("Não foi possivel encontrar a empresa com esse id")
+    else:
+        print("Não foi possivel alterar a empresa!")
+
+
+def cadastrar_empresa():
+    nome_empresa: str = questionary.text("Digite o nome da empresa: ").ask()
+    cnpj_empresa: str = questionary.text("Digite o CNPJ da empresa: ").ask()
+
+    empresa = {
+        "nome": nome_empresa,
+        "cnpj": cnpj_empresa
+    }
+
+    resposta = requests.post(url, json=empresa)
+    if resposta.status_code == 201:
+        print("Empresa cadastrada com sucesso!!")
+    else:
+        print("Não foi possivel cadastrar a empresa!!")
+
+
+def consultar_empresa_cnpj():
+    cnpj_selecionado = questionary.text("Digite o cnpj para consultar: ").ask()
+
+    resposta = requests.get(url)
+
+    console = Console()
+
+    table = Table(show_header=True, header_style="bold blue", show_lines=True)
+    table.add_column("ID")
+    table.add_column("Nome")
+    table.add_column("CNPJ")
+
+    empresas = resposta.json()
+
+    for empresa in empresas:
+        for chave, valor in empresa.items():
+            if chave == "cnpj":
+                if valor == cnpj_selecionado:
+                    table.add_row(str(empresa["id"]), empresa["nome"], empresa["cnpj"])
+    
+    console.print(table)
+
+
+def consultar_empresa_nome():
+    nome_selecionado = questionary.text("Digite o nome para consultar esta empresa: ").ask()
+
+    resposta = requests.get(url)
+
+    console = Console()
+
+    table = Table(show_header=True, header_style="bold blue", show_lines=True)
+    table.add_column("ID")
+    table.add_column("Nome")
+    table.add_column("CNPJ")
+
+    empresas = resposta.json()
+
+    for empresa in empresas:
+        for chave, valor in empresa.items():
+            if chave == "nome":
+                if valor == nome_selecionado:
+                    table.add_row(str(empresa["id"]), empresa["nome"], empresa["cnpj"])
+    
+    console.print(table)
+
+
+def consultar_empresa_id():
+    id_selecionado = int(questionary.text("Digite o id para consultar esta empresa: ").ask())
+
+    resposta = requests.get(url)
+
+    console = Console()
+
+    table = Table(show_header=True, header_style="bold blue", show_lines=True)
+    table.add_column("ID")
+    table.add_column("Nome")
+    table.add_column("CNPJ")
+
+    empresas = resposta.json()
+
+    for empresa in empresas:
+        for chave, valor in empresa.items():
+            if chave == "id":
+                if valor == id_selecionado:
+                    table.add_row(str(empresa["id"]), empresa["nome"], empresa["cnpj"])
+    
+    console.print(table)
+
+
+
+def consultar_empresas():
+    resposta = requests.get(url)
+
+    console = Console()
+
+    table = Table(show_header=True, header_style="bold blue", show_lines=True)
+    table.add_column("ID")
+    table.add_column("Nome")
+    table.add_column("CNPJ")
+
+    empresas = resposta.json()
+
+    for empresa in empresas:
+        table.add_row(str(empresa["id"]), empresa["nome"], empresa["cnpj"])
+
+    console.print(table)
+
+
+def limpar_terminal():
+    os.system("clear")
     
 
-    
+menu()
